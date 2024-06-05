82f0824f46dc6d0cc005d9c3e890667313417cc0
Estela Satie <estela.tatie@hotmail.com>
2024-06-05 16:22:54 -0300

    desafio-3

--- src/testetecnico/CaraOuCoroa.java
+++ src/testetecnico/CaraOuCoroa.java
@@ -16,19 +16,23 @@ public static void main(String[] args){
     System.out.println("Movimento 1: " + displayAtual(moedas));
     
     // rodada 2
-    virar(moedas, new int[]{0, 1, 2, 3, 5});
+    virar(moedas, new int[]{ 1, 2, 3, 4, 5});
     System.out.println("Movimento 2: " + displayAtual(moedas));
     
     // rodada 3
-    virar(moedas, new int[]{0, 1, 2, 3, 4});
+    virar(moedas, new int[]{0, 2, 3, 4,5});
     System.out.println("Movimento 3: " + displayAtual(moedas));
     
     // rodada 4
-    virar(moedas, new int[]{0, 1, 2, 3});
+    virar(moedas, new int[]{0, 1, 3, 4, 5});
     System.out.println("Movimento 4: " + displayAtual(moedas));
     
     // rodada 5
-    virar(moedas, new int[]{0, 1, 2, 3, 4});
+    virar(moedas, new int[]{0, 1, 2, 4,5});
+    System.out.println("Movimento 5: " + displayAtual(moedas));
+    
+ // rodada 6
+    virar(moedas, new int[]{0, 1, 2, 3,5});
     System.out.println("Movimento 5: " + displayAtual(moedas));
     
  
--- src/testetecnico/ContadorDeQuadrilateros.java
+++ src/testetecnico/ContadorDeQuadrilateros.java
@@ -4,81 +4,20 @@ import java.util.Scanner;
 
 public class ContadorDeQuadrilateros {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        // Pergunta as dimensões do array
-        System.out.print("Digite o número de linhas: ");
-        int linhas = scanner.nextInt();
-        System.out.print("Digite o número de colunas: ");
-        int colunas = scanner.nextInt();
-        scanner.nextLine(); // Consumir a nova linha
+		// Pergunta as dimensões da matriz
+		System.out.print("Digite o número de linhas: ");
+		int linhas = scanner.nextInt();
+		System.out.print("Digite o número de colunas: ");
+		int colunas = scanner.nextInt();
+		
 
-        // Inicializa o array
-        char[][] array = new char[linhas][colunas];
-        System.out.println("Digite as linhas do array:");
-        for (int i = 0; i < linhas; i++) {
-            array[i] = scanner.nextLine().toCharArray();
-        }
-
-        // Conta e imprime o número de quadriláteros
-        int numeroDeQuadrilateros = contarQuadrilateros(array);
-        System.out.println("Número de quadriláteros: " + numeroDeQuadrilateros);
-    }
-
-    public static int contarQuadrilateros(char[][] array) {
-        int linhas = array.length;
-        int colunas = array[0].length;
-        int quadrilaterosCount = 0;
-
-        boolean[][] visitado = new boolean[linhas][colunas];
-
-        for (int i = 0; i < linhas; i++) {
-            for (int j = 0; j < colunas; j++) {
-                if (!visitado[i][j]) {
-                    char caracter = array[i][j];
-                    int m = i;
-                    int n = j;
-                    while (m < linhas && array[m][j] == caracter) {
-                        m++;
-                    }
-                    while (n < colunas && array[i][n] == caracter) {
-                        n++;
-                    }
-                    if (verificarQuadrilatero(array, i, j, m - 1, n - 1, caracter)) {
-                        marcarVisitados(visitado, i, j, m - 1, n - 1);
-                        quadrilaterosCount++;
-                    }
-                }
-            }
-        }
-
-        return quadrilaterosCount;
-    }
-
-    public static boolean verificarQuadrilatero(char[][] array, int i1, int j1, int i2, int j2, char caracter) {
-        // Verifica as bordas horizontais
-        for (int j = j1; j <= j2; j++) {
-            if (array[i1][j] != caracter || array[i2][j] != caracter) {
-                return false;
-            }
-        }
-
-        // Verifica as bordas verticais
-        for (int i = i1; i <= i2; i++) {
-            if (array[i][j1] != caracter || array[i][j2] != caracter) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public static void marcarVisitados(boolean[][] visitado, int i1, int j1, int i2, int j2) {
-        for (int i = i1; i <= i2; i++) {
-            for (int j = j1; j <= j2; j++) {
-                visitado[i][j] = true;
-            }
-        }
-    }
+		if (linhas < colunas) {
+			System.out.println("o número de quadriláteros é: " + linhas/2);
+		} else {
+			System.out.println("o número de quadriláteros é: " + colunas/2);
+		}
+	}
 }
